e9b633ad0f55ed8c2206cc9a66cb226cccd8ec1b
ologuiearlette <arletteologuie@gmail.com>
2026-01-02 20:23:59 +0100

    project setup and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { MovieProvider } from './context/MovieContext';
+// import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // <MovieProvider>
+      <Router>
+        <div className="min-h-screen bg-[#0F1014] text-white">
+          {/* Navbar ici */}
+          <Routes>
+            <Route path="/" element={<div>Home</div>} />
+            <Route path="/movie/:id" element={<div>Details</div>} />
+            <Route path="/bookmarks" element={<div>Favorites</div>} />
+          </Routes>
+        </div>
+      </Router>
+    // </MovieProvider>
+  );
 }
 
-export default App
+export default App;
