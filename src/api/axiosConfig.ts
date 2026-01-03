4f0944f65cd6d97a4ef8656edbc5028b2ff3d38d
ologuiearlette <arletteologuie@gmail.com>
2026-01-03 14:28:23 +0100

    adding console.log to view bug issues

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL || "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_TMDB_BASE_URL,
+    baseURL: BASE_URL,
     params: {
-        api_key: import.meta.env.VITE_TMDB_API_KEY,
+        api_key: API_KEY,
         language: 'en-US',
     },
 });
@@ -11,7 +14,8 @@ const api = axios.create({
 api.interceptors.response.use(
     (response) => response,
     (error) => {
-        console.error("API Error:", error.response?.data?.status_message || error.message);
+        console.error("URL appelée :", error.config?.url);
+        console.error("BaseURL :", error.config?.baseURL);
         return Promise.reject(error);
     }
 );
